3c0847fdd0b2d4aba697a3aaccb1d3a44fc9973f
Mahardika Angky <mahardika.sid@gmail.com>
2026-02-17 19:09:10 +0700

    Rate Aggregator | init project

---
+++ src/main/java/com/allobank/angky/rateaggregatorservice/RateAggregatorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.allobank.angky.rateaggregatorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateAggregatorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RateAggregatorServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/allobank/angky/rateaggregatorservice/RateAggregatorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allobank.angky.rateaggregatorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateAggregatorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
